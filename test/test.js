63971c7a8a16d199ddeae36bca79e4069fa0309b
Magno Gouveia <magno.gouveia@metasix.com.br>
2017-07-11 09:22:04 -0300

    First Test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+'use strict'
+const chai = require('chai')
+const main = require('../js/main')
+
+describe('Main', () => {
+    it('Should do something', () => {
+        console.log('oi')
+    })
+})
