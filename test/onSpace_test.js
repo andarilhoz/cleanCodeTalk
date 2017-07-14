1284a5c44a7d245ebb0ddbf620d1fd7b98bd9e90
Magno Gouveia <magno.gouveia@metasix.com.br>
2017-07-14 10:41:59 -0300

    Removed out of context tests

--- test/onSpace_test.js
+++ test/onSpace_test.js
@@ -34,14 +34,6 @@ define(function(require){
             Manager.player.jumping.should.be.equal(false)
         })
 
-        it('should set actualPos to 0 when gameOver is true', () => {
-            Manager.player.personagemAltitude = 500
-            Manager.gameOver = true
-            fn = () => {}
-            onSpace(fn)
-            Manager.player.personagemAltitude.should.be.equal(0)
-        })
-
         it('should change high score if difference val is greater and gameOver is true', () => {
             localStorage.setItem('HighScore',0)
             Manager.gameOver = true
@@ -49,22 +41,6 @@ define(function(require){
             fn = () => {}
             onSpace(fn)
             parseInt(Manager.highScore).should.be.equal(10)            
-        })
-
-        it('should reset date when gameOver is true', async () => {
-            Manager.gameOver = true
-            await sleepTest(100)
-            fn = () => {}
-            onSpace(fn)
-            let dateTest = new Date()
-            let differenceBetweenDates = dateTest - Manager.initialGameTime
-            differenceBetweenDates.should.be.lessThan(10)
-        })
-
-        it('should execute callback when given and gameOver is true', (done) => {
-            Manager.gameOver = true
-            fn = () => {done()}
-            onSpace(fn)
         })
 
     })
